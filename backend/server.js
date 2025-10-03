ad8696876f21988d0095f793ac399b45e076c57b
diegogopa <diegogopa@unisabana.edu.co>
2025-10-03 15:47:00 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();          // Cargamos nuestras variables de entorno desde nuestro archivo .env y permite q process.env.var funcione correcta,mente 
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
@@ -12,12 +12,16 @@ const FRONTEND_URL = process.env.FRONTEND_URL || '*';
 app.use(express.json());
 
 app.use(cors({
-  origin: FRONTEND_URL      //Peritimos que el fronted pueda llamar al bacjkend desde el navegador
+  origin: FRONTEND_URL
 }));
+
+// ✅ Ruta principal
+app.get("/", (req, res) => {
+  res.send("Bienvenido al Parcial 🚀");
+});
 
 app.use('/api/v1', authRoutes);
 app.use('/api/v1', pokemonRoutes);
 
-const PORT = process.env.PORT || 4000;    //Puerto donde escuchará el servido o tamnbien se puede por medio de 4000 
-app.listen(PORT, () => console.log(`Backend running on port ${PORT}`)); //Inicia y muestra el mensaje en nuestra consola
-
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
