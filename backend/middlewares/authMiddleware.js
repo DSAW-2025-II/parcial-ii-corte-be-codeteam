f7781b4be61e20677c70270f605b28112767ed5e
diegogopa <diegogopa@unisabana.edu.co>
2025-10-03 21:16:54 -0500

    Update authMiddleware.js

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,28 +1,30 @@
-const jwt = require('jsonwebtoken');  //Importa,os la libreria para poder verificar los tokens de WJT
+const jwt = require('jsonwebtoken');
 
-const JWT_SECRET = process.env.JWT_SECRET; //Lee nuestra clave desde nueestras variables de entorno, para obtener mayor privacidad
-
+const JWT_SECRET = process.env.JWT_SECRET;
 
 function authMiddleware(req, res, next) {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
-    return res.status(403).json({ error: 'User not authenticated' }); //Si no exixte autorisasion envia el error 
+    return res.status(403).json({ error: 'User not authenticated' });
   }
 
-  const parts = authHeader.split(' ');                                   // Separa en 2 nuestro token y el Bearer pq nuestro autenticacion nesesita nuestro esquema 
-    return res.status(403).json({ error: 'User not authenticated' });    // y credenciales donde el esquema es el Bearer y las credenciales los datos nesesarios para poder autenticar
+  const parts = authHeader.split(' ');
   if (parts.length !== 2 || parts[0] !== 'Bearer') {
+    return res.status(403).json({ error: 'User not authenticated' });
   }
 
   const token = parts[1];
   try {
     const payload = jwt.verify(token, JWT_SECRET);
-    req.user = payload;
-    next();                                            //Llamamos a next() para que la petición del usuario continúe hacia la ruta segura por  nosotros.
+    req.user = payload; // opcional: puedes usar payload más adelante
+    next(); // deja pasar la petición
   } catch (err) {
-    return res.status(403).json({ error: 'User not authenticated' }); //Lanza un error si el token es incorrecto o no coincide 
-
+    return res.status(403).json({ error: 'User not authenticated' });
   }
+}
+
+module.exports = authMiddleware;
+
 }
 
 module.exports = authMiddleware; //Exporta los middlewares para q puedan ser usadas en nuestrss rutas protegidas
